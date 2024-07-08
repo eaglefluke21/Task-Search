eaaf3e9a9e5c37b1a37632c2f73837d42b1264c8
eaglefluke21 <yadavankit7fc4@gmail.com>
2024-07-08 14:31:43 +0530

    service folder

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiAxios = axios.create({
-    baseURL:'http://localhost:3000',
+    baseURL:'https://task-search-node.vercel.app',
 });
 
 export default apiAxios;
